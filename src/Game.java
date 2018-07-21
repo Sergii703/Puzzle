f90a03273ac79114821759ea68a963647df5db6d
Sergii703 <sergey703@gmail.com>
2018-07-21 23:33:05 +0200

    add Game class and array list with a puzzle position

---
+++ src/Game.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class Game extends JFrame{
+    private ArrayList<Point> solution;
+    private JPanel panel;
+    private BufferedImage source;
+    private BufferedImage resized;
+    private int width, height;
+    private  final int DESIRED_WIDTH = 400;
+
+    public Game(){
+
+    }
+
+    public void initUI(){
+        solution = new ArrayList<>();
+        solution.add(new Point(0, 0));
+        solution.add(new Point(0, 1));
+        solution.add(new Point(0, 2));
+        solution.add(new Point(1, 0));
+        solution.add(new Point(1, 1));
+        solution.add(new Point(1, 2));
+        solution.add(new Point(2, 0));
+        solution.add(new Point(2, 1));
+        solution.add(new Point(2, 2));
+        solution.add(new Point(3, 0));
+        solution.add(new Point(3, 1));
+        solution.add(new Point(3, 2));
+    }
+}
